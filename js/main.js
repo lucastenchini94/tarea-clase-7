90d2194204b8ea92f50d9580ec1cde4b75958281
lucastenchini94 <lucastenchini94@gmail.com>
2024-10-22 13:44:46 -0300

    tarea-de-validaciones
    probamos validar el nombre, la seleccion de ciudad y la descripcion de regalo y a la ves hacer pruebas para confirmar que todas las validaciones funcionen correctamente

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const form = document.querySelector("#carta-a-santa");
+
+const nombre = form.nombre.value;
+const ciudad = form.ciudad.value;
+const comportamiento = form.comportamiento.value;
+const descripcionRegalo = form["descripcion-regalo"].value;
+
+
+
+function validarNombre(nombre) {
+    if(nombre.length === 0){
+    return "Este campo debe tener al menos 1 caracter";
+    }
+    if(nombre.length >= 50){
+        return "Este campo debe tener menos de 50 caracteres";
+    }
+    
+    return "";
+}
+
+function validarCiudad(ciudad) {
+    if (ciudad === "") {
+        return "porfavor tiene que seleccionar una opcion para continuar";
+    }
+}
+
+function validarDescripcionRegalo(descripcionRegalo) {
+    if (descripcionRegalo.length<=10) {
+        return "este campo tiene que tener al menos 10 caracteres o mas";
+    }
+
+
+}
--- js/pruebas.js
+++ js/pruebas.js
@@ -1,3 +1,4 @@
+
 function probarValidarNombre() {
   console.assert(
       validarNombre('') === 'Este campo debe tener al menos 1 caracter',
@@ -11,5 +12,18 @@ function probarValidarNombre() {
       'Validar nombre no validó que el nombre sea menor a 50 caracteres',
   );
 }
-
+function probarValidarCiudad(){
+console.assert(
+    validarCiudad("") === "porfavor tiene que seleccionar una opcion para continuar",
+    "validacion de ciudad no valido que el usuario no selecciono una ciudad",
+);
+}
+function probarValidarDescripcionRegalo(){
+console.assert(
+    validarDescripcionRegalo("1111111111") === "este campo tiene que tener al menos 10 caracteres o mas",
+    "validacion de descripicion de regalo no valido que la descripcion tenga mas de 10 caracteres",
+);
+}
 probarValidarNombre();
+probarValidarCiudad();
+probarValidarDescripcionRegalo();
